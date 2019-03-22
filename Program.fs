68c5170d8bc84df3834853e79ceac933822ec7b8
Brian Berns <brianberns@gmail.com>
2019-03-22 12:09:51 -0400

    Single step reduction now returns an option.

--- Program.fs
+++ Program.fs
@@ -3,6 +3,7 @@
  * https://www.cis.upenn.edu/~bcpierce/tapl/
  *)
 
+/// Language syntax.
 type Term =
 
     (* Boolean *)
@@ -18,45 +19,61 @@ type Term =
 
 module Term =
 
-    let rec eval term  =
+    /// Active pattern for numeric values.
+    let rec (|NumericValue|_|) = function
+        | Zero -> Some Zero
+        | Succ (NumericValue nv) -> Some (Succ nv)
+        | _ -> None
 
-        let rec (|NumericValue|_|) = function
-            | Zero -> Some Zero
-            | Succ (NumericValue nv) -> Some (Succ nv)
-            | _ -> None
+    /// Fully evaluates the given term, reducing it to normal form.
+    let rec eval term =
 
+        /// Reduces a term by one step, if it's not already in normal form.
         let rec step = function
 
-            (* Boolean *)
-            | If (True, t2, _) -> t2
-            | If (False, _, t3) -> t3
+                // boolean
+            | If (True, t2, _) -> Some t2
+            | If (False, _, t3) -> Some t3
             | If (t1, t2, t3) ->
-                let t1' = step t1
-                If (t1', t2, t3)
+                step t1
+                    |> Option.map (fun t1' ->
+                        If (t1', t2, t3))
 
-            (* Numeric *)
+                // numeric
             | Succ t1 ->
-                let t1' = step t1
-                Succ t1'
-            | Pred Zero -> Zero
+                step t1
+                    |> Option.map (fun t1' ->
+                        Succ t1')
+            | Pred Zero -> Some Zero
             | Pred (Succ (NumericValue nv1)) ->
-                nv1
+                Some nv1
             | Pred t1 ->
-                let t1' = step t1
-                Pred t1'
-            | IsZero Zero -> True
+                step t1
+                    |> Option.map (fun t1' ->
+                        Pred t1')
+            | IsZero Zero -> Some True
             | IsZero (Succ (NumericValue _)) ->
-                False
+                Some False
             | IsZero t1 ->
-                let t1' = step t1
-                IsZero t1'
+                step t1
+                    |> Option.map (fun t1' ->
+                        IsZero t1')
 
-            (* Normal form *)
-            | term -> term
+                // normal form - no further reduction possible
+            | _ -> None
 
-        let term' = step term
-        if term' = term then term
-        else eval term'
+            // evaluate the given term recursively, one step at a time
+        step term
+            |> Option.map (fun term' ->
+                eval term')
+            |> Option.defaultValue term
+
+    /// Is the given term a value?
+    let isValue = function
+        | True
+        | False -> true
+        | NumericValue _ -> true
+        | _ -> false
 
 [<EntryPoint>]
 let main argv =
@@ -66,9 +83,13 @@ let main argv =
             If (True, If (False, False, False), True)
             If (True, True, If (False, False, False))
             Pred (Succ (Pred Zero))
+            If (Zero, Zero, Zero)
         ]
     for term in terms do
         printfn ""
         printfn "%A" term
-        printfn "Eval: %A" <| Term.eval term
+        let term' = Term.eval term
+        printfn "Eval: %A" term'
+        if term' |> Term.isValue |> not then
+            printfn "*** ERROR ***"
     0
